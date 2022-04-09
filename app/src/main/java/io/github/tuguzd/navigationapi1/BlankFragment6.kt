e6fe512c6ba1d03b581aa95ae4d9d1c027779e02
Damir Tugushev <0damir.1tugushev@gmail.com>
2022-04-09 23:29:49 +0300

    Initial commit

---
+++ app/src/main/java/io/github/tuguzd/navigationapi1/BlankFragment6.kt
@@ -0,0 +1,32 @@
+package io.github.tuguzd.navigationapi1
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.findNavController
+import io.github.tuguzd.navigationapi1.databinding.FragmentBlank6Binding
+
+class BlankFragment6 : Fragment() {
+    private var _binding: FragmentBlank6Binding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?,
+    ): View {
+        _binding = FragmentBlank6Binding.inflate(inflater, container, false)
+
+        binding.ButtonBack.setOnClickListener {
+            it.findNavController().popBackStack()
+        }
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
